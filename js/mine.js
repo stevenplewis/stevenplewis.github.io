98a4db483ef4517ef218dd68f0bec4ad4831fa80
stevelewis <baffledappledesign@gmail.com>
2014-02-13 18:22:48 -0800

    new site. rewritten blurbs. compiled and all. READY FOR ACTION!!!

---
+++ js/mine.js
@@ -0,0 +1,12 @@
+// CALLING THE HISRC PLUGIN
+$(document).ready(function(){
+  $('.hisrc img').hisrc();
+});
+
+// CALLING FITTEXT ON HEADLINE
+$(document).ready(function(){
+  $('.responsive_h1').fitText(10, { minFontSize: '70px', maxFontSize: '100px' })
+
+  $('.responsive_h2').fitText(22, { minFontSize: '20px', maxFontSize: '35px' })
+  $('.responsive_links').fitText(15, { minFontSize: '25px', maxFontSize: '200px' })
+});
