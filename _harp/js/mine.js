073ca3a8167689acd0c33245f60b5c5d6b1ddec8
stevelewis <baffledappledesign@gmail.com>
2014-02-18 00:58:36 -0800

    changed subhead and the color of links, as per Nate's suggestions.

--- _harp/js/mine.js
+++ _harp/js/mine.js
@@ -7,6 +7,6 @@ $(document).ready(function(){
 $(document).ready(function(){
   $('.responsive_h1').fitText(10, { minFontSize: '70px', maxFontSize: '100px' })
 
-  $('.responsive_h2').fitText(22, { minFontSize: '20px', maxFontSize: '35px' })
+  $('.responsive_h2').fitText(23, { minFontSize: '28px', maxFontSize: '35px' })
   $('.responsive_links').fitText(15, { minFontSize: '25px', maxFontSize: '200px' })
 });
