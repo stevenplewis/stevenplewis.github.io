2bebf8b0ca0a80febbb6ab5dd1529a2321d74a0f
stevelewis <baffledappledesign@gmail.com>
2014-02-25 01:05:30 -0800

    updated comic-shelf

--- js/comic-shelf.js
+++ js/comic-shelf.js
@@ -1,9 +1,14 @@
 function kimonoCallback(data) {
+// begin kimon functions and looping over issues
+
   var newComics = data.results.collection1
   
   console.log(newComics.length);
 
+  // looping over all the issues
   for (var i = 0; i < newComics.length; i+=1) {
+    // logs for all the different values we're getting
+
     // console.log(newComics[i]);
     // console.log(newComics[i].issue_title.text);
     // console.log(newComics[i].issue_title.href);
@@ -12,21 +17,38 @@ function kimonoCallback(data) {
     // console.log(newComics[i].issue_info);
     // console.log(newComics[i].cover_art.src);
 
+    // getting the larger-res image for awesome browsing.
     var coverBig = newComics[i].cover_art.src.replace('/n_iv/120/', '/n_iv/300/');
 
-    $('ul').append('<li><img src="' + coverBig + '"><div><h2>' + newComics[i].issue_title.text + '</h2><h3>' + newComics[i].issue_num + '</h3><p>' + newComics[i].issue_info + '</p></div></li>');
-  };
-}
+    // creating the HTML for each li or "issue"
+    $('#comic-list').append('<li><img src="' + coverBig + '"><div class="info-div"><h2>' + newComics[i].issue_title.text + '</h2><h3>' + newComics[i].issue_num.replace('#ITEM', '') + '</h3><p>' + newComics[i].issue_info + '</p></div></li>');
 
+  // end of looping over all the issues
+  };
+
+  // click listener to toggle class
+  // has to come after loop to make sure they're all loaded
+  $('#comic-list li').click(function(){
+    if ( $(this).hasClass('selected') ) {
+      $(this).removeClass('selected');
+    } else {
+      $('.selected').removeClass('selected');
+      $(this).addClass('selected').find('div');
+      $('body').animate({scrollTop:$('.selected').offset().top},500);
+    }
+  });
+
+
+
+// end of kimono stuff
+};
+
+
+
+// some other kimono nonsense
 $.ajax({
     "url":"http://www.kimonolabs.com/api/71blnv5q?apikey=d62eaf8c48b7eb0fe3cab0f0dc1bab83&callback=kimonoCallback",
     "crossDomain":true,
     "dataType":"jsonp"
 });
-
-
-$(function () {
-});
-
-
 
